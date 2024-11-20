5aba9b4541a92f2363ec0648fa0e069df42cbbab
louisajohansson <louisa.j.johansson1@gmail.com>
2024-11-20 11:36:59 +0100

    Updated js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function pressButton(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-bar-input");
+  let h1 = document.querySelector("#name-of-city");
+  h1 = city.value;
+}
+
+let button = document.querySelector("#search-bar-search");
+button.addEventListener("click", pressButton);
