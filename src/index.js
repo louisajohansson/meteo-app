bad36a3099fb2ff4143e408e605655224036dc8c
louisajohansson <louisa.j.johansson1@gmail.com>
2024-11-20 12:10:55 +0100

    Updated js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function currentTemp(response) {
+  let currentTemp = document.querySelector("#current-temp");
+let temperature = response.data.temperature.current;
+  let h1 = document.querySelector("#city");
+  h1.innerHTML = response.data.city;
+  currentTemp.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "fac344c0db5f6d5ctc9743db656e4obf";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metrics`;
+  axios.get(apiURL).then(currentTemp);
+}
+
 function pressButton(event) {
   event.preventDefault();
   let city = document.querySelector("#search-bar-input");
-  let h1 = document.querySelector("#name-of-city");
-  h1.innerHTML = city.value;
+  
+  searchCity(city.value);
 }
 
 let button = document.querySelector("#search-bar-search");
 button.addEventListener("click", pressButton);
+
+searchCity("New York");
