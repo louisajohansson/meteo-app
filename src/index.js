2ef590948bd356f71cc60118e7eafd91436f152b
louisajohansson <louisa.j.johansson1@gmail.com>
2024-11-20 14:53:13 +0100

    Integrated API

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ function currentTemp(response) {
   currentTemp.innerHTML = Math.round(temperature);
   timeElement.innerHTML = formatDate(date);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-emoji" />`;
+  getForecast(response.data.city);
 }
 
 function formateDescription(emoji) {
@@ -61,25 +62,43 @@ function pressButton(event) {
   searchCity(city.value);
 }
 
-function displayForecast() {
-  let days = ["Tue", "Wed", "Thur", "Fri", "Sat"];
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "fac344c0db5f6d5ctc9743db656e4obf";
+  let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metrics`;
+  axios(apiURL).then(displayForecast);
+}
+
+function displayForecast(response) {
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
   <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <div >
+    <img class="weather-forecast-icon" src="${day.condition.icon_url}" />
+    </div>
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
-        <strong>15°</strong>
+        <strong>${Math.round(day.temperature.maximum)}°</strong>
       </div>
-      <div class="weather-forecast-temperature">9°</div>
+      <div class="weather-forecast-temperature">${Math.round(
+        day.temperature.minimum
+      )}°</div>
     </div>
   </div>
 `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
@@ -89,4 +108,3 @@ let button = document.querySelector("#search-bar-search");
 button.addEventListener("click", pressButton);
 
 searchCity("New York");
-displayForecast();
